bc44d04b22fb8c0e9370489b8972f95869061b23
Ashwin Anil <54780642+ASHWIN776@users.noreply.github.com>
2021-10-24 12:55:00 +0530

    Added Step - 2

--- arrays/reverse-array/reverse.js
+++ arrays/reverse-array/reverse.js
@@ -1,7 +1,16 @@
 const arr = [1,2,3,4,5];
 const rev_arr = [];
 
+// Step - 1
 for(let i = arr.length - 1; i >= 0; --i)
 {
     rev_arr.push(arr[i]);
+}
+
+// Step - 2
+for(let i = 0; i <= arr.length / 2; ++i)
+{
+    let temp = arr[i];
+    arr[i] = arr[arr.length - i - 1];
+    arr[arr.length - i - 1] = temp;
 }
